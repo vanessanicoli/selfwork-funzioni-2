c6fd6f1a6ae4f9731e811fa6d224183fe7d4578e
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-01-13 21:55:26 +0100

    Terminato Selfwork Funzioni 2

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('ciao');
+// Scrivi una funzione che permetta di stampare in console tutti i numeri da 1 a N:
+// N dovra’ rappresentare il parametro formale della funzione
+// tutti i numeri multipli di 3 siano sostituiti dalla stringa 'Fizz',
+// tutti i numeri multipli di 5 siano sostituiti dalla stringa 'Buzz'
+// e tutti i numeri multipli di 15 siano sostituiti dalla stringa 'fizzBuzz'
+
+function stampa(N){
+    for(let i=1; i<=N; i++){
+
+        if(i%15==0){
+            console.log('fizzBuzz');
+        }        
+        else if(i%5==0){
+            console.log('Buzz');
+        }
+        else if(i%3==0){
+            console.log('Fizz');
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+
+stampa(50);
